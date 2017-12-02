f18fe16ab2611449bc3707e02b5228aa5de41cfc
sartox <ls30927@salleurl.edu>
2017-12-02 11:29:08 +0100

    Opcio 2 feta. Falte generar el fitxer .json on guardar les cerques que l'usuari vulgui guardar per carregar mes tard.

---
+++ src/FavouritesModel.java
@@ -0,0 +1,51 @@
+/**
+ * Created by angel on 02/12/2017.
+ */
+public class FavouritesModel {
+    private String tipusResultat;
+    private String titol;
+    private String descripcio;
+    private String nomCanal;
+
+    public String getTipusResultat() {
+        return tipusResultat;
+    }
+
+    public void setTipusResultat(String tipusResultat) {
+        this.tipusResultat = tipusResultat;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+
+    public String getNomCanal() {
+        return nomCanal;
+    }
+
+    public void setNomCanal(String nomCanal) {
+        this.nomCanal = nomCanal;
+    }
+
+    @Override
+    public String toString() {
+        return "FavouritesModel{" +
+                "tipusResultat='" + tipusResultat + '\'' +
+                ", titol='" + titol + '\'' +
+                ", descripcio='" + descripcio + '\'' +
+                ", nomCanal='" + nomCanal + '\'' +
+                '}';
+    }
+}
--- src/Menu.java
+++ src/Menu.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * Created by angel on 29/11/2017.
  */
 public class Menu {
-    JsonReader jsonReader = new JsonReader();
+    private JsonReader jsonReader = new JsonReader();
 
     void mostraMenu() {
         int i;
@@ -58,11 +58,11 @@ public class Menu {
         System.out.println("Introduiex el que vols cercar:");
         Scanner read = new Scanner(System.in);
         String queryTerm = read.nextLine();
-        String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + queryTerm + "&key=AIzaSyCHI5qNldMo0BcX8iVv7Gnx9Zc0i1fcIQ0";
+        String URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + queryTerm + "&key=AIzaSyCHI5qNldMo0BcX8iVv7Gnx9Zc0i1fcIQ0";
         try {
-            JsonObject json = jsonReader.getJsonFromURL(url);
+            JsonObject json = jsonReader.getJsonFromURL(URL);
 
-            for (int i = 0 ; i < json.size() && i < 3; i++){
+            for (int i = 0 ; i < 3; i++){
                 System.out.println("~~~~Resultat numero " + (i + 1) + "~~~~");
                 System.out.println("    Tipus de resultat: " + json.get("items").getAsJsonArray().get(i).getAsJsonObject().get("id").getAsJsonObject().get("kind").getAsString());
                 System.out.println("    Titol: " + json.get("items").getAsJsonArray().get(i).getAsJsonObject().get("snippet").getAsJsonObject().get("title").getAsString());
@@ -75,7 +75,64 @@ public class Menu {
     }
 
     private void opcio2() {
+        System.out.println("Introduiex el que vols cercar:");
+        Scanner read = new Scanner(System.in);
+        String queryTerm = read.nextLine();
+        String URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + queryTerm.replace(" ", "-") + "&key=AIzaSyCHI5qNldMo0BcX8iVv7Gnx9Zc0i1fcIQ0&maxResults=50";
+        try {
+            JsonObject json = jsonReader.getJsonFromURL(URL);
 
+            int i = 0;
+            mostraResultats(i, json);
+
+            System.out.println("Selecciona un per desar, si vols veure els seguents, els anteriors o parar:");
+            read = new Scanner(System.in);
+            String guardar = read.nextLine();
+            do {
+
+                if (guardar.toUpperCase().equals("NEXT")){
+                    i = i + 10;
+                    mostraResultats(i, json);
+                    System.out.println("Selecciona un per desar, si vols veure els seguents, els anteriors o parar:");
+                }else if (guardar.toUpperCase().equals("BACK")){
+                    i = i - 10;
+                    mostraResultats(i, json);
+                    System.out.println("Selecciona un per desar, si vols veure els seguents, els anteriors o parar:");
+                }else if (guardar.toUpperCase().equals("STOP")){
+                    return;
+                }else if (isNumeric(guardar)){
+                    int intGuardar = Integer.parseInt(guardar);
+                    guardaCerca(intGuardar);
+                    return;
+                }else{
+                    System.out.println("Aquesta no es una opcio valida! Introdueix una opcio de nou: ");
+                }
+                guardar = read.nextLine();
+            }while (!guardar.toUpperCase().equals("STOP"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IndexOutOfBoundsException e){
+            System.err.println("Ja no hi han mes resultats de cerca!");
+        }
+    }
+
+    private void guardaCerca(int intGuardar) {
+    }
+
+    private static boolean isNumeric(String str)
+    {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
+    private void mostraResultats(int index, JsonObject json) {
+        for (int i = index; i < index + 10; i++){
+            System.out.println("~~~~Resultat numero " + (i + 1) + "~~~~");
+            System.out.println("    Tipus de resultat: " + json.get("items").getAsJsonArray().get(i).getAsJsonObject().get("id").getAsJsonObject().get("kind").getAsString());
+            System.out.println("    Titol: " + json.get("items").getAsJsonArray().get(i).getAsJsonObject().get("snippet").getAsJsonObject().get("title").getAsString());
+            System.out.println("    Descripcio: " + json.get("items").getAsJsonArray().get(i).getAsJsonObject().get("snippet").getAsJsonObject().get("description").getAsString());
+            System.out.println("    Nom del canal al que correspon: " + json.get("items").getAsJsonArray().get(i).getAsJsonObject().get("snippet").getAsJsonObject().get("channelTitle").getAsString() + "\n");
+        }
     }
 
     private void opcio3() {
